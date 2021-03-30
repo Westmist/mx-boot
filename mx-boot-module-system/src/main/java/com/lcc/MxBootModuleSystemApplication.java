bde26cd432f5751939fc2333f760253f6ad69720
Westmist <mingxuannull@gmail.com>
2021-03-30 21:16:38 +0800

    项目搭建

---
+++ mx-boot-module-system/src/main/java/com/lcc/MxBootModuleSystemApplication.java
@@ -0,0 +1,13 @@
+package com.lcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxBootModuleSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MxBootModuleSystemApplication.class, args);
+    }
+
+}
